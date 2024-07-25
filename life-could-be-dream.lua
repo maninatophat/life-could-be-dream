f64469158fcbc53061e0d93fdb05e67924080c24
sparks <161984967+sparksistaken@users.noreply.github.com>
2024-07-25 16:59:14 -0400

    Create life-could-be-dream.lua

---
+++ life-could-be-dream.lua
@@ -0,0 +1,69 @@
+def print_lyrics():
+    lyrics = """
+    Life could be a dream
+    Life could be a dream
+    Do do do do, sh-boom
+
+    Life could be a dream (sh-boom)
+    If I could take you up in paradise up above (sh-boom)
+    If you would tell me I'm the only one that you love
+    Life could be a dream, sweetheart
+
+    Hello, hello again
+    Sh-boom and hopin' we'll meet again
+    Boom (ba-boom)
+
+    Day dong da ding-dong
+    A-lang-da-lang-da-lang
+    Ah, whoa, whoa, bip
+    Ah bi-ba-do-da-dip, whoa
+
+    Life could be a dream (sh-boom)
+    If only all my precious plans would come true (sh-boom)
+    If you would let me spend my whole life lovin' you
+    Life could be a dream, sweetheart
+    (Do do do do, sh-boom)
+
+    Every time I look at you
+    Something is on my mind
+    If you do what I want you to do
+    Baby, we'd be so fine
+
+    Oh, life could be a dream, sh-boom
+    If I could take you up in paradise up above, sh-boom
+    You'd tell me, darlin', I'm the only one that you love
+    Life could be a dream, sweetheart
+    Hello, hello again, sh-boom and hopin' we'll meet again, boom (ba-boom)
+
+    Day dong da ding-dong
+    A-lang-da-lang-da-lang
+    Ah, whoa, whoa, bip
+    Ah bi-ba-do-da-dip, whoa
+
+    Life could be a dream
+    Life could be a dream, sweetheart
+
+    (Do do do do, sh-boom)
+    (Do do do do, sh-boom)
+    (Do do do do, sh-boom)
+    (Do do do do, sh-boom)
+    (Do do do do, sh-boom)
+    (Do do do do, sh-boom)
+    (Do do do do, sh-boom)
+
+    Woah, life could be a dream, sh-boom
+    If I could take you up in paradise up above, sh-boom
+    You'd tell me, darlin', I'm the only one that you love
+    Life could be a dream, sweetheart
+    Hello, hello again, sh-boom and hopin' we'll meet again, boom (ba-boom)
+
+    Day dong da ding-dong
+    Sha-lang-a-la-lang-a-la-lang-a-la-lang-a-la
+    Whoa, whoa, bip
+    Ah bo da do da dip, whoa
+    Life could be a dream, life could be a dream
+    Do do do do, sh-boom
+    """
+    print(lyrics.strip())
+
+print_lyrics()
